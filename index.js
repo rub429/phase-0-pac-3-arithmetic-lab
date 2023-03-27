13e8a625a5445f20b2ea5bf38401df17507d854f
Ruben <rub429@gmail.com>
2023-03-26 22:16:13 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function add() {
+    // 
+  }
+function subtract() {
+    // 
+  }
+function multiply() {
+    // 
+  }
+function divide() {
+    // 
+  }
+function add(a, b) {
+    return "something";
+  }
+function add(a, b) {
+    return a + b;
+  }
+function subtract(a, b) {
+    return a - b;
+  }
+function multiply(a, b) {
+    return a * b;
+  }
+function divide(a, b) {
+    return a / b;
+  }
+parseInt("2", 10); //=> 2
+parseInt("2.2222", 10);
+parseInt("nonsense!", 10);
